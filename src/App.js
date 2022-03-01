782fe045a979c24cb7817d6320a557ac6d309d6d
MauroArielCoria <91713335+MauroArielCoria@users.noreply.github.com>
2022-03-01 16:01:49 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navbar from "./components/NavBar"
+import ItemListContainer from "./components/ItemListContainer"
+import Footer from "./components/Footer"
+
+const App =() => {
+    return (
+        <>
+            <Navbar/>
+            <ItemListContainer/>
+            <Footer/>
+        </>
+    )
+}
+
+
+export default App
